d7af83f8d73d936de0b9827ee8a27aeff99f9b15
shani__tripathi <shani.tripathi01@gmail.com>
2020-11-19 10:05:15 +0000

    Padding added

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 const prev = document.querySelector(".prev-btn");
 const next = document.querySelector(".next-btn");
 const images = document.querySelectorAll(".slider-col img");
-const amount = 300;
+const amount = 330;
 const totalWidth = images.length * 200;
 
 // slidler animation
@@ -45,12 +45,11 @@ const collapses = document.querySelectorAll(".collapse");
 function active() {
   setInterval(function () {
     collapses.forEach((c, index) => {
+      let element = c.parentElement.children[0].children[0].children[1];
       if (c.classList.contains("show")) {
-        let element = c.parentElement.children[0].children[0].children[1];
         element.children[0].style.display = "none";
         element.children[1].style.display = "block";
       } else {
-        let element = c.parentElement.children[0].children[0].children[1];
         element.children[0].style.display = "block";
         element.children[1].style.display = "none";
       }
